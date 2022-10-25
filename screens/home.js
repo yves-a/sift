ea679807c6a1a3f0558fcd64a676cc653f27c402
gradypalfrey <gradypalfrey@gmail.com>
2022-10-25 14:35:19 -0400

    Update home

--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Team 2's React Native App</Text>
+      {/* <Text>Welcome to Team 2's React Native App</Text> */}
+      <Image
+        source={
+          {
+            // uri: "https://m.media-amazon.com/images/I/61kqkYoeysL._AC_UL640_FMwebp_QL65_.jpg",
+          }
+        }
+        style={{ width: 400, height: 400 }}
+      />
     </View>
   );
 };
