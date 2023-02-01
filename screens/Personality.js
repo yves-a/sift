c0f8d922a3623576130bd9efa63ad3ba20facf89
Yves <yalikalfic@yahoo.com>
2023-02-01 17:03:57 -0500

    updated bug on personality screen

--- screens/Personality.js
+++ screens/Personality.js
@@ -1,28 +1,28 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import React, { useState } from "react";
-import CirculeButton from "../components/CircleButton";
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+import React, { useState } from 'react'
+import CirculeButton from '../components/CircleButton'
 
 const Personality = ({ navigation, route }) => {
-  const { recipient } = route.params;
+  const { recipient } = route.params
 
-  const [extraverted, setextraverted] = useState(false);
-  const [intraverted, setintraverted] = useState(false);
-  const [outdoorsy, setoutdoorsy] = useState(false);
-  const [creative, setcreative] = useState(false);
-  const [motivated, setmotivated] = useState(false);
-  const [chill, setchill] = useState(false);
-  const [workaholic, setworkaholic] = useState(false);
-  const [active, setactive] = useState(false);
-  const [simple, setsimple] = useState(false);
-  const [value, setValue] = useState([]);
+  const [extraverted, setextraverted] = useState(false)
+  const [intraverted, setintraverted] = useState(false)
+  const [outdoorsy, setoutdoorsy] = useState(false)
+  const [creative, setcreative] = useState(false)
+  const [motivated, setmotivated] = useState(false)
+  const [chill, setchill] = useState(false)
+  const [workaholic, setworkaholic] = useState(false)
+  const [active, setactive] = useState(false)
+  const [simple, setsimple] = useState(false)
+  const [value, setValue] = useState([])
   const updateValues = (interest) => {
     if (value.includes(interest)) {
-      setValue(value.filter((item) => item !== interest));
+      setValue(value.filter((item) => item !== interest))
     } else {
-      setValue([...value, interest]);
+      setValue([...value, interest])
     }
-  };
+  }
 
   return (
     <View style={styles.container}>
@@ -32,7 +32,7 @@ const Personality = ({ navigation, route }) => {
       <View style={styles.backButton}>
         <Pressable
           onPress={() => {
-            navigation.goBack();
+            navigation.goBack()
           }}
           title="Go to Page"
           accessibilityLabel="Go back a page."
@@ -45,105 +45,105 @@ const Personality = ({ navigation, route }) => {
 
         <View style={{ top: 20 }}>
           <Pressable
-            style={{ position: "absolute", left: -25, top: 100 }}
+            style={{ position: 'absolute', left: -25, top: 100 }}
             onPress={() => {
-              setintraverted(!intraverted);
-              updateValues("intraverted");
+              setintraverted(!intraverted)
+              updateValues('intraverted')
             }}
           >
             <CirculeButton
-              text={"Intraverted"}
+              text={'Intraverted'}
               size={135}
               pressed={intraverted}
             />
           </Pressable>
 
           <Pressable
-            style={{ position: "absolute", left: -5, top: -30 }}
+            style={{ position: 'absolute', left: -5, top: -30 }}
             onPress={() => {
-              setchill(!chill);
-              updateValues("chill");
+              setchill(!chill)
+              updateValues('chill')
             }}
           >
-            <CirculeButton text={"Chill"} size={125} pressed={chill} />
+            <CirculeButton text={'Chill'} size={125} pressed={chill} />
           </Pressable>
 
           <Pressable
-            style={{ position: "absolute", left: 220, top: -80 }}
+            style={{ position: 'absolute', left: 220, top: -80 }}
             onPress={() => {
-              setworkaholic(!workaholic);
-              updateValues("workaholic");
+              setworkaholic(!workaholic)
+              updateValues('workaholic')
             }}
           >
             <CirculeButton
-              text={"Workaholic"}
+              text={'Workaholic'}
               size={125}
               pressed={workaholic}
             />
           </Pressable>
 
           <Pressable
-            style={{ position: "absolute", left: 105, top: 165 }}
+            style={{ position: 'absolute', left: 105, top: 165 }}
             onPress={() => {
-              setextraverted(!extraverted);
-              updateValues("extraverted");
+              setextraverted(!extraverted)
+              updateValues('extraverted')
             }}
           >
             <CirculeButton
-              text={"Extraverted"}
+              text={'Extraverted'}
               size={130}
               pressed={extraverted}
             />
           </Pressable>
 
           <Pressable
-            style={{ position: "absolute", left: 100, top: 310 }}
+            style={{ position: 'absolute', left: 100, top: 310 }}
             onPress={() => {
-              setoutdoorsy(!outdoorsy);
-              updateValues("outdoorsy");
+              setoutdoorsy(!outdoorsy)
+              updateValues('outdoorsy')
             }}
           >
-            <CirculeButton text={"Outdoorsy"} size={115} pressed={outdoorsy} />
+            <CirculeButton text={'Outdoorsy'} size={115} pressed={outdoorsy} />
           </Pressable>
 
           <Pressable
-            style={{ position: "absolute", left: 120, top: 15 }}
+            style={{ position: 'absolute', left: 120, top: 15 }}
             onPress={() => {
-              setsimple(!simple);
-              updateValues("simple");
+              setsimple(!simple)
+              updateValues('simple')
             }}
           >
-            <CirculeButton text={"Simple"} size={130} pressed={simple} />
+            <CirculeButton text={'Simple'} size={130} pressed={simple} />
           </Pressable>
 
           <Pressable
-            style={{ position: "absolute", left: 230, top: 115 }}
+            style={{ position: 'absolute', left: 230, top: 115 }}
             onPress={() => {
-              setmotivated(!motivated);
-              updateValues("motivated");
+              setmotivated(!motivated)
+              updateValues('motivated')
             }}
           >
-            <CirculeButton text={"Motivated"} size={120} pressed={motivated} />
+            <CirculeButton text={'Motivated'} size={120} pressed={motivated} />
           </Pressable>
 
           <Pressable
-            style={{ position: "absolute", left: -30, top: 240 }}
+            style={{ position: 'absolute', left: -30, top: 240 }}
             onPress={() => {
-              setcreative(!creative);
-              updateValues("creative");
+              setcreative(!creative)
+              updateValues('creative')
             }}
           >
-            <CirculeButton text={"Creative"} size={130} pressed={creative} />
+            <CirculeButton text={'Creative'} size={130} pressed={creative} />
           </Pressable>
 
           <Pressable
-            style={{ position: "absolute", left: 220, top: 250 }}
+            style={{ position: 'absolute', left: 220, top: 250 }}
             onPress={() => {
-              setactive(!active);
-              updateValues("active");
+              setactive(!active)
+              updateValues('active')
             }}
           >
-            <CirculeButton text={"Active"} size={120} pressed={active} />
+            <CirculeButton text={'Active'} size={120} pressed={active} />
           </Pressable>
         </View>
 
@@ -160,20 +160,20 @@ const Personality = ({ navigation, route }) => {
                   chill ||
                   workaholic ||
                   active ||
-                  simple) == ""
+                  simple) == ''
                   ? 0.3
                   : 1,
             },
             styles.nextButton,
           ]}
           onPress={() => {
-            if (value != "") {
-              navigation.navigate("InterestsV2", {
+            if (value != '') {
+              navigation.navigate('InterestsV2', {
                 recipient: {
                   ...recipient,
                   interests: value,
                 },
-              });
+              })
             }
           }}
           title="Next"
@@ -183,60 +183,61 @@ const Personality = ({ navigation, route }) => {
         </Pressable>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default Personality;
+export default Personality
 
 const styles = StyleSheet.create({
   progressEvent: {
     flex: 0.5,
-    backgroundColor: "#333333",
-    height: "100%",
+    backgroundColor: '#333333',
+    height: '100%',
   },
   progressBar: {
-    flexDirection: "row",
-    backgroundColor: "#e0e0de",
-    width: "100%",
+    flexDirection: 'row',
+    backgroundColor: '#e0e0de',
+    width: '100%',
     height: 15,
   },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
     // justifyContent: "top",
-    top: "7%",
+    top: '7%',
   },
   backButton: {
     flex: 0.2,
-    right: "35%",
-    top: "5%",
+    right: '35%',
+    top: '5%',
   },
   body: {
     flex: 0.5,
-    width: "80%",
-    top: -80,
+    width: '80%',
+    top: '-7%',
   },
   header: {
     flexShrink: 1,
     fontSize: 40,
-    color: "#1C1B1F",
-    paddingVertical: "6%",
+    color: '#1C1B1F',
+    paddingTop: '-5%',
+    paddingBottom: '20%',
   },
   nextButton: {
-    color: "white",
+    color: 'white',
     borderRadius: 10,
     paddingVertical: 12,
     paddingHorizontal: 32,
-    top: "110%",
-    backgroundColor: "#2F3956",
+    top: '110%',
+    backgroundColor: '#2F3956',
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     letterSpacing: 0.25,
-    color: "white",
-    textAlign: "center",
+    color: 'white',
+    textAlign: 'center',
   },
-});
+})
