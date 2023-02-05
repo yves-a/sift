583d8ebd808998992fbc6a772bc97f28de356246
gradypalfrey <gradypalfrey@gmail.com>
2023-02-05 09:59:20 -0500

    Formatting changes

--- screens/Login.js
+++ screens/Login.js
@@ -1,54 +1,54 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { Formik } from 'formik'
-import styles from './LoginInFlow.style.js'
-import { AntDesign } from '@expo/vector-icons'
-import { auth, signInWithEmailAndPassword } from '../firebase'
-import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { Formik } from "formik";
+import styles from "./LoginInFlow.style.js";
+import { AntDesign } from "@expo/vector-icons";
+import { auth, signInWithEmailAndPassword } from "../firebase";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 // const auth = getAuth()
 
 const Login = ({ navigation }) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleSignIn = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        const user = userCredential.user
-        console.log('Logged in!')
-        navigation.navigate('Loading')
+        const user = userCredential.user;
+        console.log("Logged in!");
+        navigation.navigate("Loading");
       })
       .catch((error) => {
-        setError('Password or email is incorrect!')
-        console.log(error)
-      })
-  }
+        setError("Password or email is incorrect!");
+        console.log(error);
+      });
+  };
 
   const handleResetPassword = () => {
     sendPasswordResetEmail(auth, email)
       .then(() => {
-        setError('Please check your email!')
+        setError("Please check your email!");
       })
       .catch((error) => {
-        if (error.code === 'auth/missing-email') {
-          setError('Please enter an email, then try again :)')
+        if (error.code === "auth/missing-email") {
+          setError("Please enter an email, then try again :)");
         }
         if (
-          error.code === 'auth/invalid-email' ||
-          error.code === 'auth/user-not-found'
+          error.code === "auth/invalid-email" ||
+          error.code === "auth/user-not-found"
         ) {
-          setError('No account attached to this email')
+          setError("No account attached to this email");
         }
-      })
-  }
+      });
+  };
 
   return (
     <View style={styles.container}>
       <TouchableOpacity
         onPress={() => {
-          navigation.goBack()
+          navigation.goBack();
         }}
         style={styles.tinyLogo}
       >
@@ -81,12 +81,12 @@ const Login = ({ navigation }) => {
         >
           <Text style={styles.formButtonText}>Sign In</Text>
         </TouchableOpacity>
-        <Text style={{ paddingLeft: '2%' }}>{error}</Text>
+        <Text style={{ paddingLeft: "2%" }}>{error}</Text>
       </View>
       {/* )} */}
       {/* </Formik> */}
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- screens/RecipientName.js
+++ screens/RecipientName.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import {
   Pressable,
   SafeAreaView,
@@ -7,36 +7,36 @@ import {
   Text,
   View,
   TouchableOpacity,
-} from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const RecipientName = ({ navigation }) => {
-  const [textInputName, setTextInputName] = useState('')
+  const [textInputName, setTextInputName] = useState("");
 
   const checkTextInput = () => {
     if (!textInputName.trim()) {
-      alert('Please Enter Name')
-      return
+      alert("Please Enter Name");
+      return;
     } else {
-      navigation.navigate('PurchaseDate', {
+      navigation.navigate("PurchaseDate", {
         recipient: {
           name: textInputName.trim(),
         },
-      })
+      });
     }
-  }
+  };
 
   useEffect(() => {
     navigation.getParent()?.setOptions({
       tabBarStyle: {
-        display: 'none',
+        display: "none",
       },
-    })
+    });
     return () =>
       navigation.getParent()?.setOptions({
         tabBarStyle: undefined,
-      })
-  }, [navigation])
+      });
+  }, [navigation]);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -46,7 +46,7 @@ const RecipientName = ({ navigation }) => {
       <View style={styles.backButton}>
         <Pressable
           onPress={() => {
-            navigation.goBack()
+            navigation.goBack();
           }}
           title="Go to Page"
           accessibilityLabel="Go back a page."
@@ -64,13 +64,13 @@ const RecipientName = ({ navigation }) => {
       />
       <Pressable
         onPress={() => {
-          if (textInputName != '') {
-            checkTextInput()
+          if (textInputName != "") {
+            checkTextInput();
           }
         }}
         style={({ pressed }) => [
           {
-            opacity: pressed || textInputName == '' ? 0.3 : 1,
+            opacity: pressed || textInputName == "" ? 0.3 : 1,
           },
           styles.nextButton,
         ]}
@@ -80,76 +80,76 @@ const RecipientName = ({ navigation }) => {
         <Text style={styles.text}>Next</Text>
       </Pressable>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   progressEvent: {
     flex: 0.0,
-    backgroundColor: '#333333',
-    height: '100%',
+    backgroundColor: "#333333",
+    height: "100%",
   },
   progressBar: {
-    flexDirection: 'row',
-    backgroundColor: '#e0e0de',
-    width: '100%',
+    flexDirection: "row",
+    backgroundColor: "#e0e0de",
+    width: "100%",
     height: 15,
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     // alignItems: 'center',
     // justifyContent: "top",
-    top: '7%',
+    top: "7%",
   },
   buttonActive: {
-    backgroundColor: 'red',
+    backgroundColor: "red",
   },
   backButton: {
     flex: 0.2,
-    right: '36%',
-    top: '5%',
-    alignSelf: 'center',
+    right: "36%",
+    top: "5%",
+    alignSelf: "center",
   },
   head: {
     fontSize: 45,
     lineHeight: 52,
-    left: '10%',
+    left: "10%",
     marginTop: 175,
-    width: '80%',
+    width: "80%",
   },
   input: {
-    width: '80%',
+    width: "80%",
     height: 50,
-    left: '10%',
+    left: "10%",
     marginTop: 40,
-    justifyContent: 'center',
+    justifyContent: "center",
     fontSize: 18,
-    alignItems: 'center',
-    color: 'black',
-    borderBottomColor: 'black',
+    alignItems: "center",
+    color: "black",
+    borderBottomColor: "black",
     borderBottomWidth: 3,
     padding: 10,
-    color: 'black',
+    color: "black",
   },
   nextButton: {
-    color: 'white',
+    color: "white",
     borderRadius: 10,
     paddingVertical: 12,
     paddingHorizontal: 32,
     marginTop: 40,
-    width: '85%',
-    left: '7.5%',
-    backgroundColor: '#2F3956',
+    width: "85%",
+    left: "7.5%",
+    backgroundColor: "#2F3956",
   },
   text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
   },
-})
+});
 
-export default RecipientName
+export default RecipientName;
